1c00e5992db9901219512212a4cf2e5c3e06f0f9
jezekon <ondrejjezek@seznam.cz>
2023-08-01 17:24:12 +0200

    runtests - popis

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,19 +12,21 @@ using JLD
 
     # Data from Matlab:
     data = matread(taskName * ".mat")
+    # data = matread("test/" * taskName * ".mat")
     rho = vec(data["rho"])
     mesh = data["msh"]
-    X = mesh["X"]
-    X = [X[:, i] for i = 1:size(X, 2)]
-    IEN = convert(Array{Int64}, mesh["IEN"] .+ 1)
-    IEN = [IEN[:, i] for i = 1:size(IEN, 2)]
+    X = mesh["X"] # Matrix
+    X = [X[:, i] for i in axes(X,2)]
+    IEN = convert(Array{Int64}, mesh["IEN"] .+ 1) # Matrix
+    IEN = [IEN[:, i] for i in axes(IEN, 2)] # Vector{Vector{Int64}}
 
     # input data propertis (mesh, density)
-    mesh = Rho2sdf.Mesh(X, IEN)
+    mesh = Rho2sdf.Mesh(X, IEN)  
     ρₙ = Rho2sdf.elementToNodalValues(mesh, rho) # nodal values calculation (AVERAGE!! -> least squares)
 
+    # face triangular mesh from 
+    mesh = Rho2sdf.extractSurfaceTriangularMesh(mesh, ρₙ) # přepsání X a IEN (pro trojúhelníky)
 
-    mesh = Rho2sdf.extractSurfaceTriangularMesh(mesh, ρₙ)
     # save("taskName" * "_triangular_mesh.jld", "mesh", mesh)
     # mesh = load("taskName" * "_triangular_mesh.jld", "mesh") # načtení chapadla (stl)
 
@@ -32,13 +34,13 @@ using JLD
     # IEN = [mesh.IEN[:,i] for i in 1:size(mesh.IEN,2)]
     # Rho2sdf.exportToVTU("triChapadlo.vtu", X, IEN)
 
-    X_min, X_max = Rho2sdf.getMesh_AABB(mesh.X)
+    X_min, X_max = Rho2sdf.getMesh_AABB(mesh.X) # vec, vec
     N = [300, 300, 300]
-    sdf_grid = Rho2sdf.Grid(X_min, X_max, N)
+    sdf_grid = Rho2sdf.Grid(X_min, X_max, N) # cartesian grid
 
     ρₜ = 0.5
 
-    sdf_dists = Rho2sdf.evalSignedDiscancesOnTriangularMesh(mesh, sdf_grid)
+    sdf_dists = Rho2sdf.evalSignedDiscancesOnTriangularMesh(mesh, sdf_grid) # Vector{Float64}
     # sdf_dists = Rho2sdf.evalSignedDiscances(mesh, sdf_grid, ρₙ , ρₜ)
 
     Rho2sdf.exportStructuredPointsToVTK(taskName*"_sdf.vtk", sdf_grid, sdf_dists, "distance")
