e7db54e822a3bb689b377f53988fb3693e4c35e3
jezekon <78500480+jezekon@users.noreply.github.com>
2023-10-05 22:54:10 +0200

    Update Single_derivatives.jl
    Dokončeno (možná obsahuje chyby)
    - doplněny derivace
    - naformátováno
    - ověřena funkčnost zjednodušeného zápisu

--- src/Derivatives/Single_derivatives.jl
+++ src/Derivatives/Single_derivatives.jl
@@ -3,56 +3,89 @@ function dn_dΞ_compute(
     norm_dρ_dΞ::Float64,
     dρ_dΞ::Vector)
 
-    Z = zeros(Flot64, 3, 3)
-    @einsum Z[i, j] := (dρ_dΞ[i] * d²ρ_dΞ²[j, k] * dρ_dΞ[k])
+    Z = zeros(Float64, 3, 3)
+    @einsum Z[i, j] = (dρ_dΞ[i] * d²ρ_dΞ²[j, k] * dρ_dΞ[k])
 
-    dn_dΞ = zeros(Float64, 3, 3)
-    @einsum dn_dΞ[i, j] := # dve tečky když matice neni alokovaná
-        d²ρ_dΞ²[i, j] / norm_dρ_dΞ - # ověřena práce se skalárem -> OK
-        Z / norm_dρ_dΞ^3  # ok
+    dn_dΞ = d²ρ_dΞ² ./ norm_dρ_dΞ - Z ./ norm_dρ_dΞ^3  # ok testovano
 
-    test = d²ρ_dΞ² ./ norm_dρ_dΞ - # ověřena práce se skalárem -> OK
-           Z ./ norm_dρ_dΞ^3  # ok
-    test == dn_dΞ
     return dn_dΞ
 end
 
-function dd_dΞ_compute(dx_dΞ::Matrix, n::Vector, x::Vector, xₚ::Vector, dn_dΞ::Matrix)
+function dd_dΞ_compute(
+    dx_dΞ::Matrix,
+    n::Vector,
+    x::Vector,
+    xₚ::Vector,
+    dn_dΞ::Matrix
+)
+
+    X = x - xₚ
+
     dd_dΞ = zeros(Float64, 3)
-    @einsum dd_dΞ[i] :=
-        -dx_dΞ[i, k] * n[k] +
-        (x[k] - xₚ[k]) * dn_dΞ[k, i] # ok pro Tensors a Einsum
+    @einsum dd_dΞ[i] = -dx_dΞ[i, k] * n[k] + X[k] * dn_dΞ[k, i] # ok pro Tensors a Einsum
+
     return dd_dΞ
+end
+
+function d²x_dΞ²_compute(
+    Xₑ::Matrix,
+    d²N_dξ²::Array
+)
+
+    d²x_dΞ² = zeros(Float64, 3, 3, 3)
+    @einsum d²x_dΞ²[i, j, k] = Xₑ[i, m] * d²N_dξ²[m, j, k] # ok
+
+    return d²x_dΞ²
 end
 
 
 function d²n_dΞ²_compute(
-d³ρ_dΞ³::Array,
-norm_dρ_dΞ::Float64,
-d²ρ_dΞ²::Matrix,
-dρ_dΞ::Vector,
+    d³ρ_dΞ³::Array,
+    norm_dρ_dΞ::Float64,
+    d²ρ_dΞ²::Matrix,
+    dρ_dΞ::Vector,
 )
 
     Z1 = zeros(Float64, 3, 3, 3)
-    @einsum Z1[i, j, k] := (d²ρ_dΞ²[i, j] * d²ρ_dΞ²[k, m] * dρ_dΞ[m]) 
-    
-    Z2 = zeros(Float64, 3, 3)
-    @einsum Z2[j, k] := dρ_dΞ[i] * (d²ρ_dΞ²[j, m] * d²ρ_dΞ²[m, k] +
-        d³ρ_dΞ³[j, k, m] * dρ_dΞ[m])
-    
+    @einsum Z1[i, j, k] = d²ρ_dΞ²[i, j] * d²ρ_dΞ²[k, m] * dρ_dΞ[m] # ok 
+
+    # Z2 = zeros(Float64, 3, 3, 3)
+    # @einsum Z2[i, j, k] := dρ_dΞ[i] * (d²ρ_dΞ²[j, m] * d²ρ_dΞ²[m, k] +
+    # d³ρ_dΞ³[j, k, m] * dρ_dΞ[m]) # přepsat abych to chápal
+
+    Z2 = zeros(Float64, 3, 3, 3)
+    @einsum Z2[i, j, k] = d²ρ_dΞ²[i, j] * dρ_dΞ[l] * d²ρ_dΞ²[l, k] +
+                           dρ_dΞ[i] * d²ρ_dΞ²[j, m] * d²ρ_dΞ²[m, k] +
+                           dρ_dΞ[i] * dρ_dΞ[m] * d³ρ_dΞ³[j, k, m]
+
     Z3 = zeros(Float64, 3, 3, 3)
-    @einsum Z3[i, j, k] := dρ_dΞ[i] * dρ_dΞ[m] * d²ρ_dΞ²[m, j] * dρ_dΞ[l] * d²ρ_dΞ²[l, k]
-    
-    d²n_dΞ² = zeros(Float64, 3, 3, 3)
-    @einsum d²n_dΞ²[i, j, k] :=
-        d³ρ_dΞ³[i, j, k] / norm_dρ_dΞ -
-        Z1 / norm_dρ_dΞ^3 -
-        Z2 / norm_dρ_dΞ^3 +
-        3 * Z3 / norm_dρ_dΞ^5 # ok
+    @einsum Z3[i, j, k] = dρ_dΞ[i] * dρ_dΞ[m] * d²ρ_dΞ²[m, j] * dρ_dΞ[l] * d²ρ_dΞ²[l, k] # ok
+
+    d²n_dΞ² = d³ρ_dΞ³ ./ norm_dρ_dΞ -
+              Z1 ./ norm_dρ_dΞ^3 -
+              Z2 ./ norm_dρ_dΞ^3 +
+              3 .* Z3 ./ norm_dρ_dΞ^5 # ok testovano
 
     return d²n_dΞ²
 end
 
+function d²d_dΞ²_compute(
+    x::Vector,
+    xₚ::Vector,
+    d²x_dΞ²::Array,
+    n::Vector,
+    dx_dΞ::Matrix,
+    dn_dΞ::Matrix,
+    d²n_dΞ²::Array,
+)
 
+    X = x - xₚ
 
+    d²d_dΞ² = zeros(Float64, 3, 3)
+    @einsum d²d_dΞ²[i, j] = -d²x_dΞ²[i, j, k] * n[k] -
+        2 * dx_dΞ[i, k] * dn_dΞ[k, j] +
+        X[k] * d²n_dΞ²[k, i, j]
+
+    return d²d_dΞ²
+end
 
