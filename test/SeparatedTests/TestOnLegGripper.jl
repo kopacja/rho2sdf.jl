9074b93c404170af66e33340d0e88907dbc85654
jezekon <78500480+jezekon@users.noreply.github.com>
2024-01-30 20:24:51 +0100

    Update TestOnLegGripper.jl

--- test/SeparatedTests/TestOnLegGripper.jl
+++ test/SeparatedTests/TestOnLegGripper.jl
@@ -6,6 +6,7 @@ using Rho2sdf.PrimitiveGeometries
 using Rho2sdf.ShapeFunctions
 using Rho2sdf.MeshGrid
 using Rho2sdf.SignedDistances
+using Rho2sdf.DataExport
 using MAT
 using SymPy
 using LinearAlgebra
@@ -13,30 +14,53 @@ using JLD
     
     # Data from Matlab:
     taskName = "chapadlo"
-    data = matread(taskName * ".mat")
+    # data = matread(taskName * ".mat")
+    data = matread("test/" * taskName * ".mat")
     
-    part_name = "elementy_trubky.txt"
+    # part_name = "elementy_trubky.txt"
+    part_name = "test/elementy_trubky.txt"
 
     (X, IEN, rho) = MeshGrid.MeshInformations(data)
 
     # input data propertis (mesh, density)
     mesh = MeshGrid.Mesh(X, IEN, C3D8_SFaD)
     (mesh, rho) = MeshGrid.PartOfModel(mesh, rho, part_name)
-    rho = MeshGrid.ModiffElementalDensities(mesh, rho) # change density along the object
+    # rho = MeshGrid.ModiffElementalDensities(mesh, rho) # change density along the object
 
     ρₙ = MeshGrid.DenseInNodes(mesh, rho) # LSQ
     ## Grid:
     X_min, X_max = MeshGrid.getMesh_AABB(mesh.X) # vec, vec
     
     N = 20  #Number of divisions along the longest side (along some axis)
-    sdf_grid = MeshGrid.Grid(X_min, X_max, N) # cartesian grid
+    sdf_grid = MeshGrid.Grid(X_min, X_max, N, 3) # cartesian grid
     
     ## SDF from densities:
     ρₜ = 0.5
     (sdf_dists, xp) = SignedDistances.evalSignedDistances(mesh, sdf_grid, ρₙ, ρₜ)
+    points = generateGridPoints(sdf_grid)
+    Xg, Xp, mean_PD, max_PD = SignedDistances.SelectProjectedNodes(mesh, sdf_grid, xp, points) # PD (=projected distance)
+
+    nnp = Int(length(Xg)/2)
+    IEN = [[i; i + nnp] for i = 1:nnp]
+
+    nnp₂ = Int(length(Xg)/2)
+    IEN₂ = [[i; i + nnp₂] for i = 1:nnp₂]
+
+    # X_combined = [Xg; Xp] 
+    # X_combined_couples = [X Xp]
+
+    nnp = length(Xg)
+    IEN = [[i; i + nnp] for i = 1:nnp]
+
+    DataExport.exportToVTU("xp.vtu", X, IEN, 5)
+
+    # Rho2sdf.exportToVTU("xp.vtu", X_combined, IEN)
+    DataExport.exportToVTU("Xg.vtu", Xg, IEN₂, 5)
+    DataExport.exportToVTU("Xp.vtu", Xp, IEN₂, 5)
+
 
     ## Data export to VTK:
-    Rho2sdf.exportStructuredPointsToVTK("sdf2-leg-" * taskName *"_sdf.vtk", sdf_grid, sdf_dists, "distance")
+    # Rho2sdf.exportStructuredPointsToVTK("sdf2-leg-" * taskName *"_sdf.vtk", sdf_grid, sdf_dists, "distance")
 
 
 end
