6d0d9c337107c04b4f684b30681fce68424b1de8
jezekon <78500480+jezekon@users.noreply.github.com>
2024-05-22 21:29:17 +0800

    add MC_SurfaceTriangularMesh - not tested

--- src/MyMarchingCubes/MC4surfaceMesh.jl
+++ src/MyMarchingCubes/MC4surfaceMesh.jl
@@ -11,7 +11,7 @@ function MC_OnCube(ρₑ::Vector{Float64},
   ρ = [ρₑ[1], ρₑ[2], ρₑ[4], ρₑ[3], ρₑ[5], ρₑ[6], ρₑ[8], ρₑ[7]] .- ρₜ
   Y = reshape(ρ, (2, 2, 2)) # Array for MC algorithm
 
-  mc = MC(Y, Int; x=[-1., 1.], y=[-1., 1.], z=[-1., 1.]) # cube inicialization (MC not working with general mesh, but with grid)
+  mc = MC(Y, Int; x=[-1.0, 1.0], y=[-1.0, 1.0], z=[-1.0, 1.0]) # cube inicialization (MC not working with general mesh, but with grid)
 
   # Perform MC:
   march(mc)
@@ -129,52 +129,104 @@ end
 
 # Function to assign coordinates to each node
 function assign_coordinates(
-    triangles,
-    coordinates::Vector{Vector{Float64}}
-    )
+  triangles,
+  coordinates::Vector{Vector{Float64}}
+)
 
-    # Initialize the result as a vector of vectors of vectors
-    result = Vector{Vector{Vector{Float64}}}(undef, length(triangles))
+  # Initialize the result as a vector of vectors of vectors
+  result = Vector{Vector{Vector{Float64}}}(undef, length(triangles))
 
-    # Iterate over each triangle
-    for (i, triangle) in enumerate(triangles)
-        # Initialize the coordinate vector for this triangle
-        triangle_coords = Vector{Vector{Float64}}(undef, 3)
-        
-        # Map each node ID to its corresponding coordinates
-        for (j, node_id) in enumerate(triangle)
-            triangle_coords[j] = coordinates[node_id]
-        end
+  # Iterate over each triangle
+  for (i, triangle) in enumerate(triangles)
+    # Initialize the coordinate vector for this triangle
+    triangle_coords = Vector{Vector{Float64}}(undef, 3)
 
-        # Assign the coordinates to the result vector
-        result[i] = triangle_coords
+    # Map each node ID to its corresponding coordinates
+    for (j, node_id) in enumerate(triangle)
+      triangle_coords[j] = coordinates[node_id]
     end
 
-    return result
+    # Assign the coordinates to the result vector
+    result[i] = triangle_coords
+  end
+
+  return result
 end
 
-# Call the function
-result = assign_coordinates(triangles, vertices_coords)
+# result = assign_coordinates(triangles, vertices_coords)
 
 
 function MC4trianglesInElement(
-    ρₑ::Vector,
-    ρₜ::Float64,
-    mesh::MGMesh,
-    Xₑ::Matrix,
-    )
-    
-    # ISE = mesh.ISE
-    nsd = mesh.nsd
-    edges = mesh.edges
+  ρₑ::Vector,
+  ρₜ::Float64,
+  mesh::MGMesh,
+  Xₑ::Matrix,
+)
 
-    # Cutting triangles single cube element based on nodal densities (MS algorithm):
-    (triangles, vertices) =  MC_OnCube(ρₑ, ρₜ) # POTŘEBUJI
+  # ISE = mesh.ISE
+  nsd = mesh.nsd
+  edges = mesh.edges
 
-    # Transform vertices coords from cube to real element:
-    (vertices_coords, ID_edges) = EdgeIdDetection(vertices, Xₑ, edges, nsd) # POTŘEBUJI vertices_coords
+  # Cutting triangles single cube element based on nodal densities (MS algorithm):
+  (triangles, vertices) = MC_OnCube(ρₑ, ρₜ) # POTŘEBUJI
 
-    Coords4Triangles = assign_coordinates(triangles, vertices_coords)
+  # Transform vertices coords from cube to real element:
+  (vertices_coords, ID_edges) = EdgeIdDetection(vertices, Xₑ, edges, nsd) # POTŘEBUJI vertices_coords
 
-  return Coords4Triangles
+  Coords4Triangles = assign_coordinates(triangles, vertices_coords)
+
+  return (Coords4Triangles, triangles)
+end
+
+function MC_SurfaceTriangularMesh(
+  ρₙ::Vector{Float64},
+  ρₜ::Float64,
+  mesh::MGMesh,
+  Xₑ::Matrix,
+)
+
+  X = mesh.X
+  IEN = mesh.IEN # ID element -> nodes
+  INE = mesh.INE # ID node -> ID elements
+  ISN = mesh.ISN # connectivity face - edges
+  nsd = mesh.nsd # number of special dimension
+  nel = mesh.nel # number of elements
+  nes = mesh.nes # number of element segments (faces) 6
+  nsn = mesh.nsn # number of segment nodes (kolik má stěna uzlů) 4
+  sfce = mesh.sfce
+
+  X_new = Vector{Vector{Float64}}()
+  # push!(X_new, vec(X[:, 1]))
+
+  IEN_new = Vector{Vector{Int64}}()
+
+  for el = 1:nel
+    ρₑ = ρₙ[IEN[:, el]]
+
+    ρₑ_min = minimum(ρₑ)
+    ρₑ_max = maximum(ρₑ)
+    if (ρₑ_max > ρₜ) && (ρₑ_min < ρₜ) # The boundary (isocontour) goes through the element
+      (Coords4Triangles, triangles) = MC4trianglesInElement(ρₑ, ρₜ, mesh, Xₑ)
+
+      not = length(Coords4Triangles) # number of trinagles in the element
+
+      for i in 1:not
+        Xt = Coords4Triangles[i]
+        IEN_el = zeros(Int64, 3)
+
+        for j in 1:3
+          a = findfirst(x -> norm(x - Xt[j]) < 1.0e-5, X_new)
+          if (a === nothing)
+            push!(X_new, Xt[j])
+            IEN_el[j] = length(X_new)
+          else
+            IEN_el[j] = a[1]
+          end
+        end
+        push!(IEN_new, IEN_el)  # This should be outside the inner loop
+      end
+    end
+  end # el
+
+  return (X_new, IEN_new)
 end
