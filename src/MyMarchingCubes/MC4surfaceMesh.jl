d32be892cad76b17513f00c7dd55f4f7c0edc755
jezekon <78500480+jezekon@users.noreply.github.com>
2024-05-24 14:06:19 +0800

    VertCoord for real elements
    using ShapeFunctions

--- src/MyMarchingCubes/MC4surfaceMesh.jl
+++ src/MyMarchingCubes/MC4surfaceMesh.jl
@@ -6,6 +6,7 @@
 
 function MC_OnCube(ρₑ::Vector{Float64},
   ρₜ::Float64)
+  # println("rho ele: ", ρₑ)
 
   # Sort to match my elements definitions
   ρ = [ρₑ[1], ρₑ[2], ρₑ[4], ρₑ[3], ρₑ[5], ρₑ[6], ρₑ[8], ρₑ[7]] .- ρₜ
@@ -15,117 +16,33 @@ function MC_OnCube(ρₑ::Vector{Float64},
 
   # Perform MC:
   march(mc)
+  # println("triangles: ", mc.triangles)
+  # println("vertices: ", mc.vertices)
+  # exit()
   return (mc.triangles, mc.vertices)
 end
 
 # (triangles, vertices) = MC_OnCube(ρₑ, ρₜ)
 
+function ShapeFunctionTemp(Ξ)
+  ξ₁ = Ξ[1]
+  ξ₂ = Ξ[2]
+  ξ₃ = Ξ[3]
 
-function EdgeIdDetection(
-  vertices,
-  Xₑ::Matrix,
-  edges::NTuple,
-  nsd::Int,
-)
+  # N = Array{Float64}(undef, 8)
+  N = zeros(Float64, 8)
+  N[1] = -1 / 8 * (ξ₁ - 1) * (ξ₂ - 1) * (ξ₃ - 1)
+  N[2] = 1 / 8 * (ξ₁ + 1) * (ξ₂ - 1) * (ξ₃ - 1)
+  N[3] = -1 / 8 * (ξ₁ + 1) * (ξ₂ + 1) * (ξ₃ - 1)
+  N[4] = 1 / 8 * (ξ₁ - 1) * (ξ₂ + 1) * (ξ₃ - 1)
 
-  number_of_vert = length(vertices)
-  vertices_coords = [zeros(Float64, nsd) for _ in 1:number_of_vert]
-  ID_edges = zeros(Int, number_of_vert)
-
-  # To be able to compute position, I need to know on which edge am I:
-  for i in 1:number_of_vert
-    vert = vertices[i]
-    # Cases (which edge?)
-    if vert[2] == -1.0 && vert[3] == -1.0 # edge 1
-      edge_ID = 1
-      node₁, node₂ = edges[edge_ID]
-      ratio = (vert[1] + 1) / 2
-      vert_coords = Xₑ[:, node₁] .+ (Xₑ[:, node₂] .- Xₑ[:, node₁]) .* ratio
-
-    elseif vert[1] == 1.0 && vert[3] == -1.0 # edge 2
-      edge_ID = 2
-      node₁, node₂ = edges[edge_ID]
-      ratio = (vert[2] + 1) / 2
-      vert_coords = Xₑ[:, node₁] .+ (Xₑ[:, node₂] .- Xₑ[:, node₁]) .* ratio
-
-    elseif vert[2] == 1.0 && vert[3] == -1.0 # edge 3
-      edge_ID = 3
-      node₁, node₂ = edges[edge_ID]
-      ratio = (vert[1] + 1) / 2
-      vert_coords = Xₑ[:, node₁] .+ (Xₑ[:, node₂] .- Xₑ[:, node₁]) .* ratio
-
-    elseif vert[1] == -1.0 && vert[3] == -1.0 # edge 4
-      edge_ID = 4
-      node₁, node₂ = edges[edge_ID]
-      ratio = (vert[2] + 1) / 2
-      vert_coords = Xₑ[:, node₁] .+ (Xₑ[:, node₂] .- Xₑ[:, node₁]) .* ratio
-
-    elseif vert[2] == -1.0 && vert[3] == 1.0 # edge 5
-      edge_ID = 5
-      node₁, node₂ = edges[edge_ID]
-      ratio = (vert[1] + 1) / 2
-      vert_coords = Xₑ[:, node₁] .+ (Xₑ[:, node₂] .- Xₑ[:, node₁]) .* ratio
-
-    elseif vert[1] == 1.0 && vert[3] == 1.0 # edge 6
-      edge_ID = 6
-      node₁, node₂ = edges[edge_ID]
-      ratio = (vert[2] + 1) / 2
-      vert_coords = Xₑ[:, node₁] .+ (Xₑ[:, node₂] .- Xₑ[:, node₁]) .* ratio
-
-    elseif vert[2] == 1.0 && vert[3] == 1.0 # edge 7
-      edge_ID = 7
-      node₁, node₂ = edges[edge_ID]
-      ratio = (vert[1] + 1) / 2
-      vert_coords = Xₑ[:, node₁] .+ (Xₑ[:, node₂] .- Xₑ[:, node₁]) .* ratio
-
-    elseif vert[1] == -1.0 && vert[3] == 1.0 # edge 8
-      edge_ID = 8
-      node₁, node₂ = edges[edge_ID]
-      ratio = (vert[2] + 1) / 2
-      vert_coords = Xₑ[:, node₁] .+ (Xₑ[:, node₂] .- Xₑ[:, node₁]) .* ratio
-
-    elseif vert[1] == -1.0 && vert[2] == -1.0 # edge 9
-      edge_ID = 9
-      node₁, node₂ = edges[edge_ID]
-      ratio = (vert[3] + 1) / 2
-      vert_coords = Xₑ[:, node₁] .+ (Xₑ[:, node₂] .- Xₑ[:, node₁]) .* ratio
-
-    elseif vert[1] == 1.0 && vert[2] == -1.0 # edge 10
-      edge_ID = 10
-      node₁, node₂ = edges[edge_ID]
-      ratio = (vert[3] + 1) / 2
-      vert_coords = Xₑ[:, node₁] .+ (Xₑ[:, node₂] .- Xₑ[:, node₁]) .* ratio
-
-    elseif vert[1] == 1.0 && vert[2] == 1.0 # edge 11
-      edge_ID = 11
-      node₁, node₂ = edges[edge_ID]
-      ratio = (vert[3] + 1) / 2
-      vert_coords = Xₑ[:, node₁] .+ (Xₑ[:, node₂] .- Xₑ[:, node₁]) .* ratio
-
-    elseif vert[1] == -1.0 && vert[2] == 1.0 # edge 12
-      edge_ID = 12
-      node₁, node₂ = edges[edge_ID]
-      ratio = (vert[3] + 1) / 2
-      vert_coords = Xₑ[:, node₁] .+ (Xₑ[:, node₂] .- Xₑ[:, node₁]) .* ratio
-    else
-      println("chyba při nalezení hrany")
-      continue
-    end
-
-    # Checking if the result makes sense
-    if Xₑ[:, node₁] <= vert_coords <= Xₑ[:, node₂] || Xₑ[:, node₂] <= vert_coords <= Xₑ[:, node₁]
-      # println("ok") 
-    else
-      println("chyba")
-    end
-    # Putting it all together:
-    vertices_coords[i] = vert_coords
-    ID_edges[i] = edge_ID
-  end
-  return (vertices_coords, ID_edges)
+  N[5] = 1 / 8 * (ξ₁ - 1) * (ξ₂ - 1) * (ξ₃ + 1)
+  N[6] = -1 / 8 * (ξ₁ + 1) * (ξ₂ - 1) * (ξ₃ + 1)
+  N[7] = 1 / 8 * (ξ₁ + 1) * (ξ₂ + 1) * (ξ₃ + 1)
+  N[8] = -1 / 8 * (ξ₁ - 1) * (ξ₂ + 1) * (ξ₃ + 1)
+  return N
 end
 
-# (vertices_coords, ID_edges) = EdgeIdDetection1(vertices, Xₑ, edges, nsd)
 
 # Function to assign coordinates to each node
 function assign_coordinates(
@@ -159,21 +76,20 @@ end
 function MC4trianglesInElement(
   ρₑ::Vector,
   ρₜ::Float64,
-  mesh::MGMesh,
   Xₑ::Matrix,
 )
-
-  # ISE = mesh.ISE
-  nsd = mesh.nsd
-  edges = mesh.edges
 
   # Cutting triangles single cube element based on nodal densities (MS algorithm):
   (triangles, vertices) = MC_OnCube(ρₑ, ρₜ) # POTŘEBUJI
 
   # Transform vertices coords from cube to real element:
-  (vertices_coords, ID_edges) = EdgeIdDetection(vertices, Xₑ, edges, nsd) # POTŘEBUJI vertices_coords
+  VertCoord = Vector{Vector{Float64}}(undef, length(vertices))  # Preallocate VertCoord
 
-  Coords4Triangles = assign_coordinates(triangles, vertices_coords)
+  for i in 1:length(vertices)
+    VertCoord[i] = Xₑ * ShapeFunctionTemp(vertices[i])  # Directly assign the result to each index
+  end
+
+  Coords4Triangles = assign_coordinates(triangles, VertCoord)
 
   return (Coords4Triangles, triangles)
 end
@@ -186,13 +102,7 @@ function MC_SurfaceTriangularMesh(
 
   X = mesh.X
   IEN = mesh.IEN # ID element -> nodes
-  INE = mesh.INE # ID node -> ID elements
-  ISN = mesh.ISN # connectivity face - edges
-  nsd = mesh.nsd # number of special dimension
   nel = mesh.nel # number of elements
-  nes = mesh.nes # number of element segments (faces) 6
-  nsn = mesh.nsn # number of segment nodes (kolik má stěna uzlů) 4
-  sfce = mesh.sfce
 
   X_new = Vector{Vector{Float64}}()
   # push!(X_new, vec(X[:, 1]))
@@ -207,7 +117,7 @@ function MC_SurfaceTriangularMesh(
     if (ρₑ_max > ρₜ) && (ρₑ_min < ρₜ) # The boundary (isocontour) goes through the element
 
       Xₑ = X[:, IEN[:, el]]
-      (Coords4Triangles, triangles) = MC4trianglesInElement(ρₑ, ρₜ, mesh, Xₑ)
+      (Coords4Triangles, triangles) = MC4trianglesInElement(ρₑ, ρₜ, Xₑ)
 
       not = length(Coords4Triangles) # number of trinagles in the element
 
