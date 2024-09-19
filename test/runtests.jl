6b1ef9f222f2214e8f909a81d73ed73616388bf7
jezekon <78500480+jezekon@users.noreply.github.com>
2024-09-19 16:36:22 +0800

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,181 +20,183 @@ using LinearAlgebra
   # @time @testset "MC4surfaceTests" begin include("MyMarchingCubesTests/runtests.jl") end
   #
   ### Tests on geometries: ###
-  # @testset "TestOnLegGripper" begin include("SeparatedTests/TestOnLegGripper.jl") end
+  # @testset "TestOnLegGripper" begin
+    # include("SeparatedTests/TestOnLegGripper.jl")
+  # end
   # @testset "TestOnLegGripperSTL" begin include("SeparatedTests/TestOnLegGripperSTL.jl") end
   # @testset "TestOnGripperMC" begin include("SeparatedTests/TestOnGripperMC.jl") end
   # @testset "TestOnPrimitiveGeometry" begin include("SeparatedTests/TestOnPrimitiveGeometry.jl") end
-  # end
-  # exit()
-  #     
-  # # Data from Matlab:
-  # taskName = "chapadlo"
+# end
+# exit()
+#     
+# # Data from Matlab:
+# taskName = "chapadlo"
 
-  RUN_PLANE = false
-  RUN_BLOCK = true
-  RUN_SPHERE = false
-  RUN_CHAPADLO = false
+RUN_PLANE = false
+RUN_BLOCK = false
+RUN_SPHERE = false
+RUN_CHAPADLO = true
 
-  if (RUN_PLANE)
-    @testset "Plane" begin
+if (RUN_PLANE)
+  @testset "Plane" begin
 
-      taskName = "plane"
-      N = 5  # Number of cells along the longest side
-      ρₜ = 0.5 # Threshold density (isosurface level)
+    taskName = "plane"
+    N = 15  # Number of cells along the longest side
+    ρₜ = 0.5 # Threshold density (isosurface level)
 
-      X = [
-        [-1.0, -1.0, -1.0],
-        [1.0, -1.0, -1.0],
-        [1.0, 1.0, -1.0],
-        [-1.0, 1.0, -1.0],
-        [-1.0, -1.0, 1.0],
-        [1.0, -1.0, 1.0],
-        [1.0, 1.0, 1.0],
-        [-1.0, 1.0, 1.0],
-      ]
-      IEN = [[1, 2, 3, 4, 5, 6, 7, 8]]
-      # ρₙ = [0.0, 0.0, 0.0, 0.0, 1, 1, 1, 1]
-      ρₙ = [0.0, 0.0, 0.0, 0.0, 1, 1, 1, 0.5]
-      # ρₙ = [1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0] # dve roviny 4 uzlové
-      # ρₙ = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1]
-      # ρₙ = [0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 1]
-      # ρₙ = [1.0, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 1]
-      # ρₙ = [1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0] # 1, 7
+    X = [
+      [-1.0, -1.0, -1.0],
+      [1.0, -1.0, -1.0],
+      [1.0, 1.0, -1.0],
+      [-1.0, 1.0, -1.0],
+      [-1.0, -1.0, 1.0],
+      [1.0, -1.0, 1.0],
+      [1.0, 1.0, 1.0],
+      [-1.0, 1.0, 1.0],
+    ]
+    IEN = [[1, 2, 3, 4, 5, 6, 7, 8]]
+    # ρₙ = [0.0, 0.0, 0.0, 0.0, 1, 1, 1, 1]
+    # ρₙ = [0.0, 0.0, 0.0, 0.0, 1, 1, 1, 0.5]
+    # ρₙ = [1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0] # dve roviny 4 uzlové
+    # ρₙ = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1]
+    # ρₙ = [0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 1]
+    ρₙ = [1.0, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 1]
+    # ρₙ = [1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0] # 1, 7
 
-      ## Generate FEM mesh structure:
-      mesh = MeshGrid.Mesh(X, IEN, C3D8_SFaD)
+    ## Generate FEM mesh structure:
+    mesh = MeshGrid.Mesh(X, IEN, C3D8_SFaD)
 
-      VTK_CODE = 12 # https://docs.vtk.org/en/latest/design_documents/VTKFileFormats.html
-      Rho2sdf.exportToVTU(taskName * "_nodal_densities.vtu", X, IEN, VTK_CODE, ρₙ)
+    VTK_CODE = 12 # https://docs.vtk.org/en/latest/design_documents/VTKFileFormats.html
+    Rho2sdf.exportToVTU(taskName * "_nodal_densities.vtu", X, IEN, VTK_CODE, ρₙ)
 
-      ## Grid:
-      X_min, X_max = MeshGrid.getMesh_AABB(mesh.X)
-      sdf_grid = MeshGrid.Grid(X_min, X_max, N, 3) # cartesian grid
+    ## Grid:
+    X_min, X_max = MeshGrid.getMesh_AABB(mesh.X)
+    sdf_grid = MeshGrid.Grid(X_min, X_max, N, 0) # cartesian grid
 
-      # sdf_grid.cell_size = 2.
-      # sdf_grid.ngp = 1.
+    # sdf_grid.cell_size = 2.
+    # sdf_grid.ngp = 1.
 
-      ## SDF from densities:
-      (sdf_dists, xp) = SignedDistances.evalSignedDistances(mesh, sdf_grid, ρₙ, ρₜ)
-      # println(sdf_dists)
+    ## SDF from densities:
+    (sdf_dists, xp) = SignedDistances.evalSignedDistances(mesh, sdf_grid, ρₙ, ρₜ)
+    # println(sdf_dists)
 
-      ## Export to VTK:
-      # println("sdf_grid", typeof(sdf_grid))
-      println(size(sdf_grid))
-      # println("sdf_dists", typeof(sdf_dists))
-      println(size(sdf_dists))
-      Rho2sdf.exportStructuredPointsToVTK(taskName * "_sdf.vtk", sdf_grid, sdf_dists, "distance")
-    end
+    ## Export to VTK:
+    # println("sdf_grid", typeof(sdf_grid))
+    # println(size(sdf_grid))
+    # println("sdf_dists", typeof(sdf_dists))
+    # println(size(sdf_dists))
+    Rho2sdf.exportStructuredPointsToVTK(taskName * "_sdf.vtk", sdf_grid, sdf_dists, "distance")
   end
+end
 
-  if (RUN_BLOCK)
-    @testset "Block" begin
-      ## Inputs:
-      taskName = "block"
+if (RUN_BLOCK)
+  @testset "Block" begin
+    ## Inputs:
+    taskName = "block"
 
-      N = 20  # Number of cells along the longest side
-      ρₜ = 0.5 # Threshold density (isosurface level)
+    N = 20  # Number of cells along the longest side
+    ρₜ = 0.5 # Threshold density (isosurface level)
 
-      (X, IEN, rho) = PrimitiveGeometries.selectPrimitiveGeometry("block", [2, 1, 1])
-      # ρₙ = [0.0, 0.0, 0.5, 0.5, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.5, 0.5]
+    (X, IEN, rho) = PrimitiveGeometries.selectPrimitiveGeometry("block", [2, 1, 1])
+    # ρₙ = [0.0, 0.0, 0.5, 0.5, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.5, 0.5]
 
-      mesh = MeshGrid.Mesh(X, IEN, C3D8_SFaD)
-      # ρₙ = MeshGrid.DenseInNodes(mesh, rho) # LSQ
+    mesh = MeshGrid.Mesh(X, IEN, C3D8_SFaD)
+    # ρₙ = MeshGrid.DenseInNodes(mesh, rho) # LSQ
 
-      # Modif ρₙ:
-      ρₙ = [0.0, 0.0, 0.5, 0.5, 0.5, 0.5, 1.0, 1.0, 0.0, 0.0, 0.5, 0.5]
+    # Modif ρₙ:
+    ρₙ = [0.0, 0.0, 0.5, 0.5, 0.5, 0.5, 1.0, 1.0, 0.0, 0.0, 0.5, 0.5]
 
-      VTK_CODE = 12 # https://docs.vtk.org/en/latest/design_documents/VTKFileFormats.html
-      Rho2sdf.exportToVTU(taskName * "_nodal_densities.vtu", X, IEN, VTK_CODE, ρₙ)
+    VTK_CODE = 12 # https://docs.vtk.org/en/latest/design_documents/VTKFileFormats.html
+    Rho2sdf.exportToVTU(taskName * "_nodal_densities.vtu", X, IEN, VTK_CODE, ρₙ)
 
-      ## Grid:
-      X_min, X_max = MeshGrid.getMesh_AABB(mesh.X)
-      sdf_grid = MeshGrid.Grid(X_min, X_max, N, 2) # cartesian grid
+    ## Grid:
+    X_min, X_max = MeshGrid.getMesh_AABB(mesh.X)
+    sdf_grid = MeshGrid.Grid(X_min, X_max, N, 2) # cartesian grid
 
-      sleep(5)
+    sleep(5)
 
-      ## SDF from densities:
-      (sdf_dists, xp) = SignedDistances.evalSignedDistances(mesh, sdf_grid, ρₙ, ρₜ)
+    ## SDF from densities:
+    (sdf_dists, xp) = SignedDistances.evalSignedDistances(mesh, sdf_grid, ρₙ, ρₜ)
 
-      @save "$(taskName)_sdf.jld2" sdf_dists
-      @save "$(taskName)_sdf_grid.jld2" sdf_grid
+    @save "$(taskName)_sdf.jld2" sdf_dists
+    @save "$(taskName)_sdf_grid.jld2" sdf_grid
 
-      ## Export to VTK:
-      Rho2sdf.exportStructuredPointsToVTK(taskName * "_sdf.vtk", sdf_grid, sdf_dists, "distance")
-    end
+    ## Export to VTK:
+    Rho2sdf.exportStructuredPointsToVTK(taskName * "_sdf.vtk", sdf_grid, sdf_dists, "distance")
   end
+end
 
 
-  if (RUN_SPHERE)
-    @testset "Sphere" begin
-      ## Inputs:
-      taskName = "sphere"
-      N = 60  # Number of cells along the longest side
-      ρₜ = 0.5 # Threshold density (isosurface level)
+if (RUN_SPHERE)
+  @testset "Sphere" begin
+    ## Inputs:
+    taskName = "sphere"
+    N = 60  # Number of cells along the longest side
+    ρₜ = 0.5 # Threshold density (isosurface level)
 
-      ## Read FEM mesh:
-      data = matread(taskName * ".mat")
-      # data = matread("test/" * taskName * ".mat")
-      (X, IEN, rho) = MeshGrid.MeshInformations(data)
+    ## Read FEM mesh:
+    data = matread(taskName * ".mat")
+    # data = matread("test/" * taskName * ".mat")
+    (X, IEN, rho) = MeshGrid.MeshInformations(data)
 
-      ## Generate FEM mesh structure:
-      mesh = MeshGrid.Mesh(X, IEN, C3D8_SFaD)
+    ## Generate FEM mesh structure:
+    mesh = MeshGrid.Mesh(X, IEN, C3D8_SFaD)
 
-      ## Map elemental densities to the nodes:
-      ρₙ = MeshGrid.DenseInNodes(mesh, rho) # LSQ
-      #ρₙ = MeshGrid.elementToNodalValues(mesh, rho) # average
+    ## Map elemental densities to the nodes:
+    ρₙ = MeshGrid.DenseInNodes(mesh, rho) # LSQ
+    #ρₙ = MeshGrid.elementToNodalValues(mesh, rho) # average
 
-      VTK_CODE = 12 # https://docs.vtk.org/en/latest/design_documents/VTKFileFormats.html
-      Rho2sdf.exportToVTU(taskName * "_nodal_densities.vtu", X, IEN, VTK_CODE, ρₙ)
+    VTK_CODE = 12 # https://docs.vtk.org/en/latest/design_documents/VTKFileFormats.html
+    Rho2sdf.exportToVTU(taskName * "_nodal_densities.vtu", X, IEN, VTK_CODE, ρₙ)
 
-      ## Grid:
-      X_min, X_max = MeshGrid.getMesh_AABB(mesh.X)
-      sdf_grid = MeshGrid.Grid(X_min, X_max, N, 3) # cartesian grid
+    ## Grid:
+    X_min, X_max = MeshGrid.getMesh_AABB(mesh.X)
+    sdf_grid = MeshGrid.Grid(X_min, X_max, N, 3) # cartesian grid
 
-      ## SDF from densities:
-      (sdf_dists, xp) = SignedDistances.evalSignedDistances(mesh, sdf_grid, ρₙ, ρₜ)
+    ## SDF from densities:
+    (sdf_dists, xp) = SignedDistances.evalSignedDistances(mesh, sdf_grid, ρₙ, ρₜ)
 
-      ## Export to VTK:
-      Rho2sdf.exportStructuredPointsToVTK(taskName * "_sdf.vtk", sdf_grid, sdf_dists, "distance")
-    end
+    ## Export to VTK:
+    Rho2sdf.exportStructuredPointsToVTK(taskName * "_sdf.vtk", sdf_grid, sdf_dists, "distance")
   end
+end
 
-  if (RUN_CHAPADLO)
-    @testset "Chapadlo" begin
-      ## Inputs:
-      taskName = "chapadlo"
-      N = 60  # Number of cells along the longest side
-      ρₜ = 0.5 # Threshold density (isosurface level)
+if (RUN_CHAPADLO)
+  @testset "Chapadlo" begin
+    ## Inputs:
+    taskName = "chapadlo"
+    N = 60  # Number of cells along the longest side
+    ρₜ = 0.5 # Threshold density (isosurface level)
 
-      ## Read FEM mesh:
-      data = matread(taskName * ".mat")
-      (X, IEN, rho) = MeshGrid.MeshInformations(data)
-      #Z,idx_Z = findall(x->X[3,i] > 50 for i in [1:size(X,2)])
+    ## Read FEM mesh:
+    data = matread(taskName * ".mat")
+    (X, IEN, rho) = MeshGrid.MeshInformations(data)
+    #Z,idx_Z = findall(x->X[3,i] > 50 for i in [1:size(X,2)])
 
-      ## Generate FEM mesh structure:
-      mesh = MeshGrid.Mesh(X, IEN, C3D8_SFaD)
+    ## Generate FEM mesh structure:
+    mesh = MeshGrid.Mesh(X, IEN, C3D8_SFaD)
 
-      ## Map elemental densities to the nodes:
-      ρₙ = MeshGrid.DenseInNodes(mesh, rho) # LSQ
-      #ρₙ = MeshGrid.elementToNodalValues(mesh, rho) # average
+    ## Map elemental densities to the nodes:
+    ρₙ = MeshGrid.DenseInNodes(mesh, rho) # LSQ
+    #ρₙ = MeshGrid.elementToNodalValues(mesh, rho) # average
 
-      VTK_CODE = 12 # https://docs.vtk.org/en/latest/design_documents/VTKFileFormats.html
-      Rho2sdf.exportToVTU(taskName * "_nodal_densities.vtu", X, IEN, VTK_CODE, ρₙ)
+    VTK_CODE = 12 # https://docs.vtk.org/en/latest/design_documents/VTKFileFormats.html
+    Rho2sdf.exportToVTU(taskName * "_nodal_densities.vtu", X, IEN, VTK_CODE, ρₙ)
 
-      ## Grid:
-      X_min, X_max = MeshGrid.getMesh_AABB(mesh.X)
-      X_min[2] = 0
-      X_min[3] = 50
-      sdf_grid = MeshGrid.Grid(X_min, X_max, N, 3) # cartesian grid
+    ## Grid:
+    X_min, X_max = MeshGrid.getMesh_AABB(mesh.X)
+    # X_min[2] = 0
+    # X_min[3] = 50
+    sdf_grid = MeshGrid.Grid(X_min, X_max, N, 3) # cartesian grid
 
-      ## SDF from densities:
-      (sdf_dists, xp) = SignedDistances.evalSignedDistances(mesh, sdf_grid, ρₙ, ρₜ)
+    ## SDF from densities:
+    (sdf_dists, xp) = SignedDistances.evalSignedDistances(mesh, sdf_grid, ρₙ, ρₜ)
 
-      ## Export to VTK:
-      Rho2sdf.exportStructuredPointsToVTK(taskName * "_sdf.vtk", sdf_grid, sdf_dists, "distance")
-    
-      @save "$(taskName)_sdf.jld2" sdf_dists
-      @save "$(taskName)_sdf_grid.jld2" sdf_grid
-    end
+    ## Export to VTK:
+    Rho2sdf.exportStructuredPointsToVTK(taskName * "_sdf.vtk", sdf_grid, sdf_dists, "distance")
+
+    @save "$(taskName)_cele_sdf.jld2" sdf_dists
+    @save "$(taskName)_cele_sdf_grid.jld2" sdf_grid
   end
+end
 end
