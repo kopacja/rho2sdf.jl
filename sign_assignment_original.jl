f737474aab228e11a455b2909f574c36b2bc43e6
jezekon <78500480+jezekon@users.noreply.github.com>
2024-10-14 14:23:00 +0800

    fix the problem with sign_assignment (test)

--- sign_assignment_original.jl
+++ sign_assignment_original.jl
@@ -1,34 +1,31 @@
 function compute_sign_original(points, X, INE, IEN, ρₙ, ρₜ, ngp, sfce, nel)
   signs = -1 * ones(ngp)
+  # Pre-compute AABBs for all elements
+  element_aabbs = [compute_aabb(X[:, IEN[:, el]]) for el in 1:nel]
   @threads for i in 1:ngp
     x = @view points[:, i]
+    candidate_elements = [el for el in 1:nel if is_point_inside_aabb(x, element_aabbs[el]...)]
     max_local = 10.0
-    IDmin = argmin(mapslices(norm, (X .- x), dims=1))[2] # Find ID of the node (in mesh) closest to grid point x
-    none = length(INE[IDmin]) # Number of neighboring elements (connected to the node)
-    ρₙₑ = ρₙ[IEN[:, INE[IDmin]]] # Nodal densities of none elements
-    if maximum(ρₙₑ) < ρₜ # Empty elements -> skip to next i
+    none = length(candidate_elements) # Number of neighboring elements (connected to the node)
+    ρₙₑ = ρₙ[IEN[:, candidate_elements]] # Nodal densities of none elements
+    if isempty(ρₙₑ) || maximum(ρₙₑ) < ρₜ # Empty elements -> skip to next i
       continue
     end
-    # Loop through all elements that are part of the node X[IDmin]
+    # Loop through all elements for which their AABB contains x
     for j in 1:none
-      el = INE[IDmin][j]
+      el = candidate_elements[j]
       Xₑ = @view X[:, IEN[:, el]] # Coordinates of the element nodes
-      # Compute the Axis-Aligned Bounding Box (AABB)
-      min_bounds, max_bounds = compute_aabb(Xₑ)
-      # Check if the point x is inside the AABB
-      if is_point_inside_aabb(x, min_bounds, max_bounds)
-        (Conv, local_coords) = find_local_coordinates(sfce, Xₑ, x) # Conv -> true/false (converged)
-        max_local_new = maximum(abs.(local_coords))
-        
-        if max_local_new < 1.2 && max_local > max_local_new
-          H, _, _, _ = sfce(local_coords) # Shape functions and their derivatives
-          ρₑ = ρₙ[IEN[:, el]] # Nodal densities for one element
-          ρ = H ⋅ ρₑ
-          if ρ >= ρₜ
-            signs[i] = 1.0
-          end
-          max_local = max_local_new
+      (Conv, local_coords) = find_local_coordinates(sfce, Xₑ, x) # Conv -> true/false (converged)
+      max_local_new = maximum(abs.(local_coords))
+
+      if max_local_new < 1.01 && max_local > max_local_new
+        H, _, _, _ = sfce(local_coords) # Shape functions and their derivatives
+        ρₑ = ρₙ[IEN[:, el]] # Nodal densities for one element
+        ρ = H ⋅ ρₑ
+        if ρ >= ρₜ
+          signs[i] = 1.0
         end
+        max_local = max_local_new
       end
     end
   end
