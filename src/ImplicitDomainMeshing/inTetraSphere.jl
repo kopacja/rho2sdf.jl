f4a796af1c7d78a3055ffa68f39f4b104c2ab2cd
jezekon <78500480+jezekon@users.noreply.github.com>
2024-10-29 16:26:38 +0800

    Update inTetraSphere.jl
    new struct (same as in rest code)

--- src/ImplicitDomainMeshing/inTetraSphere.jl
+++ src/ImplicitDomainMeshing/inTetraSphere.jl
@@ -2,20 +2,6 @@ using LinearAlgebra
 using WriteVTK
 using StaticArrays
 
-# Reprezentace čtyřstěnu
-struct TetrahedronElement
-    vertices::SMatrix{4,3,Float64}  
-    sdf_values::SVector{4,Float64}  
-
-    function TetrahedronElement(vertices::AbstractMatrix{Float64}, sdf_values::AbstractVector{Float64})
-        size(vertices, 1) == 4 || throw(ArgumentError("Vertices must have 4 rows"))
-        size(vertices, 2) == 3 || throw(ArgumentError("Vertices must have 3 columns"))
-        length(sdf_values) == 4 || throw(ArgumentError("SDF values must have length 4"))
-        return new(SMatrix{4,3}(vertices), SVector{4}(sdf_values))
-    end
-end
-
-# Hlavní třída pro generování sítě koule
 mutable struct SphereMesh
     nx::Int
     ny::Int
@@ -24,7 +10,12 @@ mutable struct SphereMesh
     y::Vector{Float64}
     z::Vector{Float64}
     SDF::Array{Float64,3}
-    tetrahedra::Vector{TetrahedronElement}
+    X::Vector{Vector{Float64}}    # Souřadnice použitých uzlů
+    IEN::Vector{Vector{Int64}}    # Konektivita elementů
+    node_sdf::Vector{Float64}     # SDF hodnoty pro použité uzly
+    
+    # Pomocná mapa pro sledování použitých uzlů a jejich nových indexů
+    node_map::Dict{Int64, Int64}  # původní_index => nový_index
 
     function SphereMesh(nx::Int=30, ny::Int=30, nz::Int=30)
         nx > 1 || throw(ArgumentError("nx must be > 1"))
@@ -32,8 +23,11 @@ mutable struct SphereMesh
         nz > 1 || throw(ArgumentError("nz must be > 1"))
         
         mesh = new(nx, ny, nz)
-        mesh.tetrahedra = TetrahedronElement[]
         setup_grid!(mesh)
+        mesh.node_map = Dict{Int64, Int64}()
+        mesh.X = Vector{Vector{Float64}}()
+        mesh.IEN = Vector{Vector{Int64}}()
+        mesh.node_sdf = Vector{Float64}()
         return mesh
     end
 end
@@ -50,6 +44,25 @@ function setup_grid!(mesh::SphereMesh)
     for i in 1:mesh.nx, j in 1:mesh.ny, k in 1:mesh.nz
         mesh.SDF[i,j,k] = radius - sqrt(mesh.x[i]^2 + mesh.y[j]^2 + mesh.z[k]^2)
     end
+end
+
+# Pomocná funkce pro získání původního globálního indexu uzlu z i,j,k indexů
+function get_original_node_id(mesh::SphereMesh, i::Int, j::Int, k::Int)
+    return i + (j-1)*mesh.nx + (k-1)*mesh.nx*mesh.ny
+end
+
+# Pomocná funkce pro získání nebo vytvoření nového indexu uzlu
+function get_or_create_node!(mesh::SphereMesh, i::Int, j::Int, k::Int)
+    orig_id = get_original_node_id(mesh, i, j, k)
+    
+    if !haskey(mesh.node_map, orig_id)
+        # Vytvoření nového uzlu
+        push!(mesh.X, [mesh.x[i], mesh.y[j], mesh.z[k]])
+        push!(mesh.node_sdf, mesh.SDF[i,j,k])
+        mesh.node_map[orig_id] = length(mesh.X)
+    end
+    
+    return mesh.node_map[orig_id]
 end
 
 # Získání SDF hodnot pro vrcholy buňky
@@ -70,67 +83,62 @@ function get_cell_sdf_values(mesh::SphereMesh, i::Int, j::Int, k::Int)
     )
 end
 
-# Vytvoření tetraedrů pro jeden element
-function tetrahedralize_regular_cell(mesh::SphereMesh, i::Int, j::Int, k::Int)
-    vertices = Matrix{Float64}(undef, 8, 3)
-    
-    vertices[1,:] .= [mesh.x[i],   mesh.y[j],   mesh.z[k]]    # front-bottom-left
-    vertices[2,:] .= [mesh.x[i+1], mesh.y[j],   mesh.z[k]]    # front-bottom-right
-    vertices[3,:] .= [mesh.x[i+1], mesh.y[j+1], mesh.z[k]]    # front-top-right
-    vertices[4,:] .= [mesh.x[i],   mesh.y[j+1], mesh.z[k]]    # front-top-left
-    vertices[5,:] .= [mesh.x[i],   mesh.y[j],   mesh.z[k+1]]  # back-bottom-left
-    vertices[6,:] .= [mesh.x[i+1], mesh.y[j],   mesh.z[k+1]]  # back-bottom-right
-    vertices[7,:] .= [mesh.x[i+1], mesh.y[j+1], mesh.z[k+1]]  # back-top-right
-    vertices[8,:] .= [mesh.x[i],   mesh.y[j+1], mesh.z[k+1]]  # back-top-left
-    
-    tets = SMatrix{6,4,Int}([
-        1 2 4 6;  # První čtyřstěn
-        2 3 4 6;  # Druhý čtyřstěn
-        1 4 6 8;  # Třetí čtyřstěn
-        1 6 5 8;  # Čtvrtý čtyřstěn
-        3 4 7 6;  # Pátý čtyřstěn
-        4 6 7 8   # Šestý čtyřstěn
-    ])
-    
-    return vertices, tets
-end
-
-# Zpracování jedné buňky
-function process_cell(mesh::SphereMesh, i::Int, j::Int, k::Int)
-    vertices, tets = tetrahedralize_regular_cell(mesh, i, j, k)
+# Zpracování jedné buňky a vytvoření tetraedrů
+function process_cell!(mesh::SphereMesh, i::Int, j::Int, k::Int)
+    # Získání SDF hodnot
     sdf_values = get_cell_sdf_values(mesh, i, j, k)
     
     # Kontrola, zda jsou všechny hodnoty SDF kladné
     if !all(>=(0), sdf_values)
-        return TetrahedronElement[]
+        return
     end
     
-    # Pokud jsme zde, element je celý uvnitř koule
-    result_tets = TetrahedronElement[]
+    # Získání/vytvoření indexů vrcholů buňky
+    node_ids = [
+        get_or_create_node!(mesh, i, j, k),      # front-bottom-left
+        get_or_create_node!(mesh, i+1, j, k),    # front-bottom-right
+        get_or_create_node!(mesh, i+1, j+1, k),  # front-top-right
+        get_or_create_node!(mesh, i, j+1, k),    # front-top-left
+        get_or_create_node!(mesh, i, j, k+1),    # back-bottom-left
+        get_or_create_node!(mesh, i+1, j, k+1),  # back-bottom-right
+        get_or_create_node!(mesh, i+1, j+1, k+1),# back-top-right
+        get_or_create_node!(mesh, i, j+1, k+1)   # back-top-left
+    ]
     
-    for tet_idx in 1:size(tets, 1)
-        tet_indices = view(tets, tet_idx, :)
-        tet_vertices = vertices[tet_indices, :]
-        tet_sdf = Vector(sdf_values[tet_indices])
-        push!(result_tets, TetrahedronElement(tet_vertices, tet_sdf))
+    # Definice tetraedrů
+    tet_connectivity = [
+        [1, 2, 4, 6],  # První čtyřstěn
+        [2, 3, 4, 6],  # Druhý čtyřstěn
+        [1, 4, 6, 8],  # Třetí čtyřstěn
+        [1, 6, 5, 8],  # Čtvrtý čtyřstěn
+        [3, 4, 7, 6],  # Pátý čtyřstěn
+        [4, 6, 7, 8]   # Šestý čtyřstěn
+    ]
+    
+    # Vytvoření elementů s novými ID uzlů
+    for tet in tet_connectivity
+        push!(mesh.IEN, [node_ids[i] for i in tet])
     end
-    
-    return result_tets
 end
 
 # Generování sítě
 function generate_mesh!(mesh::SphereMesh)
-    empty!(mesh.tetrahedra)
-    sizehint!(mesh.tetrahedra, mesh.nx * mesh.ny * mesh.nz * 6)
+    empty!(mesh.X)
+    empty!(mesh.IEN)
+    empty!(mesh.node_sdf)
+    empty!(mesh.node_map)
     
     for i in 1:mesh.nx-1
         for j in 1:mesh.ny-1
             for k in 1:mesh.nz-1
-                append!(mesh.tetrahedra, process_cell(mesh, i, j, k))
+                process_cell!(mesh, i, j, k)
             end
         end
     end
+    
+    @info "Vytvořeno $(length(mesh.X)) uzlů a $(length(mesh.IEN)) tetraedrů"
 end
+
 
 # Export do VTK formátu
 function export_vtk(mesh::SphereMesh, filename::String)
@@ -138,43 +146,20 @@ function export_vtk(mesh::SphereMesh, filename::String)
         filename = filename * ".vtu"
     end
     
-    points_dict = Dict{NTuple{3,Float64}, Int}()
-    points = Matrix{Float64}(undef, 3, 0)
-    sdf_values = Float64[]
+    # Převod uzlů do formátu pro VTK (3×N matice)
+    points = reduce(hcat, mesh.X)
     
-    cells = Vector{MeshCell{VTKCellTypes.VTKCellType, Vector{Int}}}()
-    sizehint!(cells, length(mesh.tetrahedra))
+    # Vytvoření VTK buněk
+    cells = [MeshCell(VTKCellTypes.VTK_TETRA, tet) for tet in mesh.IEN]
     
-    for tet in mesh.tetrahedra
-        tet_points = zeros(Int, 4)
-        
-        for i in 1:4
-            vertex = Tuple(tet.vertices[i,:])
-            
-            if !haskey(points_dict, vertex)
-                points = hcat(points, [vertex[1], vertex[2], vertex[3]])
-                push!(sdf_values, tet.sdf_values[i])
-                points_dict[vertex] = size(points, 2)
-            end
-            
-            tet_points[i] = points_dict[vertex]
-        end
-        
-        push!(cells, MeshCell(VTKCellTypes.VTK_TETRA, tet_points))
-    end
-    
-    if size(points, 2) == 0
-        @warn "No points to export!"
-        return
-    end
-    
+    # Export
     vtk = vtk_grid(filename, points, cells)
-    vtk["SDF"] = sdf_values
-    vtk["radius"] = fill(0.5, length(sdf_values))
+    vtk["SDF"] = mesh.node_sdf
+    vtk["radius"] = fill(0.5, length(mesh.node_sdf))
     vtk_save(vtk)
     
     @info "VTK soubor uložen jako: $(abspath(filename))"
-    @info "Exported $(size(points, 2)) points and $(length(cells)) tetrahedra"
+    @info "Exportováno $(length(mesh.X)) uzlů a $(length(mesh.IEN)) tetraedrů"
 end
 
 # Hlavní funkce
@@ -184,7 +169,6 @@ function main()
     generate_mesh!(mesh)
     @info "Export do VTK..."
     export_vtk(mesh, "sphere_interior_mesh")
-    @info "Hotovo! Počet tetraedrů: $(length(mesh.tetrahedra))"
 end
 
 # Spuštění
