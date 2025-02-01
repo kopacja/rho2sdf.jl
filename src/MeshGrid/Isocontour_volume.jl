b9e6dd293c27dedac3c788de790ad7b900a698bb
jezekon <78500480+jezekon@users.noreply.github.com>
2025-02-01 15:15:55 +0800

    Update Isocontour_volume.jl
    easy to change gausslegendre order

--- src/MeshGrid/Isocontour_volume.jl
+++ src/MeshGrid/Isocontour_volume.jl
@@ -1,106 +1,69 @@
-
-function C3D8_SFaD_nove(Ξ::SVector{3,Float64})
-  # Rozbalíme souřadnice pro lepší čitelnost a možné SIMD optimalizace
-  ξ₁, ξ₂, ξ₃ = Ξ
-
-  # Předpočítáme často používané výrazy pro lepší výkon
-  # Tyto hodnoty kompilátor může optimalizovat do SIMD instrukcí
-  ξ₁m = (ξ₁ - 1)  # ξ₁ minus 1
-  ξ₁p = (ξ₁ + 1)  # ξ₁ plus 1
-  ξ₂m = (ξ₂ - 1)
-  ξ₂p = (ξ₂ + 1)
-  ξ₃m = (ξ₃ - 1)
-  ξ₃p = (ξ₃ + 1)
-
-  # Vytvoříme tvarové funkce jako SVector pro SIMD optimalizace
-  N = SVector{8,Float64}(
-    -0.125 * ξ₁m * ξ₂m * ξ₃m,  # N[1]
-    0.125 * ξ₁p * ξ₂m * ξ₃m,  # N[2]
-    -0.125 * ξ₁p * ξ₂p * ξ₃m,  # N[3]
-    0.125 * ξ₁m * ξ₂p * ξ₃m,  # N[4]
-    0.125 * ξ₁m * ξ₂m * ξ₃p,  # N[5]
-    -0.125 * ξ₁p * ξ₂m * ξ₃p,  # N[6]
-    0.125 * ξ₁p * ξ₂p * ξ₃p,  # N[7]
-    -0.125 * ξ₁m * ξ₂p * ξ₃p   # N[8]
-  )
-
-  # Vytvoříme matici derivací jako statickou matici 8×3
-  # Používáme konstantu 0.125 pro lepší čitelnost
-  d¹N_dξ¹ = @SMatrix [
-    -0.125*ξ₂m*ξ₃m -0.125*ξ₁m*ξ₃m -0.125*ξ₁m*ξ₂m;
-    0.125*ξ₂m*ξ₃m 0.125*ξ₁p*ξ₃m 0.125*ξ₁p*ξ₂m;
-    -0.125*ξ₂p*ξ₃m -0.125*ξ₁p*ξ₃m -0.125*ξ₁p*ξ₂p;
-    0.125*ξ₂p*ξ₃m 0.125*ξ₁m*ξ₃m 0.125*ξ₁m*ξ₂p;
-    0.125*ξ₂m*ξ₃p 0.125*ξ₁m*ξ₃p 0.125*ξ₁m*ξ₂m;
-    -0.125*ξ₂m*ξ₃p -0.125*ξ₁p*ξ₃p -0.125*ξ₁p*ξ₂m;
-    0.125*ξ₂p*ξ₃p 0.125*ξ₁p*ξ₃p 0.125*ξ₁p*ξ₂p;
-    -0.125*ξ₂p*ξ₃p -0.125*ξ₁m*ξ₃p -0.125*ξ₁m*ξ₂p
-  ]
-
-  return N, d¹N_dξ¹
-end
-
-function calculate_isocontour_volume(mesh::Mesh,
+function calculate_isocontour_volume(
+  mesh::Mesh,
   nodal_values::Vector{Float64},
-  iso_threshold::Float64)
+  iso_threshold::Float64;
+  detailed_quad_order::Int=15,
+  simple_quad_order::Int=3
+)
   X = mesh.X
   IEN = mesh.IEN
 
-  # Předpočítáme body a váhy pro kvadraturu jako statická pole
-  gp_detailed, w_detailed = gausslegendre(25)
-  gp_detailed = SVector{25}(gp_detailed)
-  w_detailed = SVector{25}(w_detailed)
+  # Compute Gauss-Legendre points and weights based on input parameters
+  gp_detailed, w_detailed = gausslegendre(detailed_quad_order)
+  gp_detailed = SVector{detailed_quad_order}(gp_detailed)
+  w_detailed = SVector{detailed_quad_order}(w_detailed)
 
-  gp_simple, w_simple = gausslegendre(3)
-  gp_simple = SVector{3}(gp_simple)
-  w_simple = SVector{3}(w_simple)
+  gp_simple, w_simple = gausslegendre(simple_quad_order)
+  gp_simple = SVector{simple_quad_order}(gp_simple)
+  w_simple = SVector{simple_quad_order}(w_simple)
 
   num_elements = size(IEN, 2)
   total_volume = Atomic{Float64}(0.0)
 
-  # Paralelní zpracování elementů
-  @threads for elem in 1:num_elements
-    # Získáme hodnoty pro uzly elementu jako statický vektor
-    elem_values = SVector{8}([nodal_values[IEN[i, elem]] for i in 1:8])
+  # Pre-allocate arrays for shape functions and their derivatives
+  N = MVector{8,Float64}(undef)
+  dN = MMatrix{8,3,Float64}(undef)
 
+  # Parallel element processing
+  @threads for elem in 1:num_elements
+    # Get element nodal values as static vector
+    elem_values = SVector{8}([nodal_values[IEN[i, elem]] for i in 1:8])
     min_value = minimum(elem_values)
     max_value = maximum(elem_values)
 
     max_value < iso_threshold && continue
 
-    # Souřadnice uzlů uložíme jako 3×8 statickou matici
+    # Store node coordinates as 3×8 static matrix
     xe = @SMatrix [X[i, IEN[j, elem]] for i in 1:3, j in 1:8]
 
-    # Zvolíme strategii integrace
+    # Choose integration strategy based on element values
     if min_value >= iso_threshold
       gp, w = gp_simple, w_simple
-      ngp = 3
+      ngp = simple_quad_order
       check_threshold = false
     else
       gp, w = gp_detailed, w_detailed
-      ngp = 25
+      ngp = detailed_quad_order
       check_threshold = true
     end
 
     element_volume = 0.0
-
     for k in 1:ngp, j in 1:ngp, i in 1:ngp
-      # Vytvoříme lokální souřadnice jako statický vektor
+      # Create local coordinates as static vector
       local_coords = SVector{3}(gp[i], gp[j], gp[k])
 
-      # Získáme tvarové funkce a jejich derivace
-      N, dN = C3D8_SFaD_nove(local_coords)
+      # Compute shape functions and their derivatives
+      Rho2sdf.SignedDistances.compute_shape_functions!(N, dN, local_coords[1], local_coords[2], local_coords[3])
 
       if check_threshold
         interpolated_value = dot(N, elem_values)
         interpolated_value < iso_threshold && continue
       end
 
-      # Opravený výpočet Jakobiánu
-      # xe je 3×8 matice, dN je 8×3 matice, výsledek bude 3×3 matice
-      J = xe * dN  # Násobíme matici 3×8 s maticí 8×3, dostaneme 3×3
+      # Calculate Jacobian (3×8 matrix × 8×3 matrix = 3×3 matrix)
+      J = xe * dN
 
-      # Přidáme příspěvek k objemu elementu
+      # Add contribution to element volume
       element_volume += w[i] * w[j] * w[k] * abs(det(J))
     end
 
@@ -161,7 +124,6 @@ function find_threshold_for_volume(mesh::Mesh,
 
     # Check convergence
     if volume_error < tolerance
-      println("\nSolution found!")
       break
     end
 
@@ -183,8 +145,7 @@ function find_threshold_for_volume(mesh::Mesh,
 
   # Print final result
   final_volume = calculate_isocontour_volume(mesh, nodal_values, best_threshold)
-  println("\nResult:")
-  print_success("Volume fraction: $(round(best_threshold, sigdigits=6))")
+  print_success("\nVolume fraction: $(round(best_threshold, sigdigits=6))")
   println("Achieved volume: $(round(final_volume, sigdigits=6))")
   print_data("Relative error: $(round(best_volume_error, sigdigits=6))")
 
