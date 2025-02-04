4472a600b82857230aefb6581a99e258238355f1
jezekon <78500480+jezekon@users.noreply.github.com>
2025-02-04 21:30:55 +0800

    Update Isosurface_stuffing_A15.jl
    new Schlafli scheme

--- src/ImplicitDomainMeshing/Isosurface_stuffing_A15.jl
+++ src/ImplicitDomainMeshing/Isosurface_stuffing_A15.jl
@@ -5,7 +5,7 @@ using JLD2
 using LinearAlgebra
 
 # Globální tolerance pro slučování uzlů (můžete upravit podle velikosti elementů)
-const TOL = 1e-8
+const TOL = 1e-6
 
 # ----------------------------
 # Struktura blokové sítě (rozšířená o node_hash)
@@ -22,8 +22,7 @@ mutable struct BlockMesh
   node_sdf::Vector{Float64}             # SDF hodnoty pro uzly
   node_map::Dict{Int64,Int64}           # mapování původního indexu gridového uzlu -> nový index
   cell_center_map::Dict{Tuple{Int,Int,Int},Int64}  # mapování buňkových středu (Steiner body)
-  # Globální slovník pro aktuální uzly – klíč je kvantizovaná n-tice souřadnic
-  node_hash::Dict{NTuple{3,Float64},Int64}  
+  node_hash::Dict{NTuple{3,Float64},Int64}  # globální slovník pro slučování uzlů
 
   function BlockMesh()
     @load "src/ImplicitDomainMeshing/data/Z_block_FineGrid.jld2" fine_grid
@@ -68,52 +67,59 @@ function get_cell_sdf_values(mesh::BlockMesh, i::Int, j::Int, k::Int)
 end
 
 # ----------------------------
-# NOVÉ: Diskretizace buňky pomocí A15 schématu
+# NOVÉ: Definice Schlafli orthoscheme
 # ----------------------------
-include("A15_scheme.jl")  # Obsahuje: tile_ref & tetra_connectivity
+# Tetraedrická konektivita – používáme pouze osmi vrcholů buňky
+const schlafli_tet_connectivity = [
+  [1, 2, 3, 7],  # Cesta 1: x, y, z
+  [1, 6, 2, 7],  # Cesta 2: x, z, y
+  [1, 3, 4, 7],  # Cesta 3: y, x, z
+  [1, 4, 8, 7],  # Cesta 4: y, z, x
+  [1, 5, 6, 7],  # Cesta 5: z, x, y
+  [1, 8, 5, 7]   # Cesta 6: z, y, x
+]
 
+# ----------------------------
+# Původní A15 schéma
+# ----------------------------
+include("A15_scheme.jl")  # Obsahuje definice tile_ref a tetra_connectivity pro A15
+
+# ----------------------------
+# Kvantizace souřadnic – stejná funkce jako dříve
+# ----------------------------
 function quantize(p::Vector{Float64}, tol::Float64=TOL)
-  # Kvantizujeme každou souřadnici – vynásobíme inverzní tolerancí, zaokrouhlíme a opět vynásobíme tol
   return (round(p[1] / tol)*tol, round(p[2] / tol)*tol, round(p[3] / tol)*tol)
 end
 
+# ----------------------------
+# Funkce diskretizace buňky pomocí A15 schématu (nezměněno)
+# ----------------------------
 function process_cell_A15!(mesh::BlockMesh, i::Int, j::Int, k::Int)
-  # Získáme SDF hodnoty osmi vrcholů buňky; pokud žádná není aktivní, buňka se přeskočí
   sdf_values = get_cell_sdf_values(mesh, i, j, k)
   if !any(x -> x >= 0, sdf_values)
     return
   end
 
-  # Určíme dolní a horní hranici buňky (předpokládáme pravoúhlou buňku)
   v000 = mesh.grid[i,   j,   k]
   v111 = mesh.grid[i+1, j+1, k+1]
   vmins = v000
   vmaxs = v111
   
-  # Získáme SDF hodnoty na vrcholech buňky
   cell_sdf = get_cell_sdf_values(mesh, i, j, k)
   f000, f100, f110, f010 = cell_sdf[1], cell_sdf[2], cell_sdf[3], cell_sdf[4]
   f001, f101, f111, f011 = cell_sdf[5], cell_sdf[6], cell_sdf[7], cell_sdf[8]
   
-  # Lokální mapování: lokální index (v rámci buňky) -> globální index
   local_mapping = Dict{Int, Int}()
   
-  # Pro každý uzel referenčního elementu (tile_ref) mapujeme do fyzikální soustavy buňky
   for li in 1:length(tile_ref)
-    # Normalizované souřadnice (tile_ref je definováno na intervalu [0,5], dělíme 4.0)
-    ξηζ = tile_ref[li] / 4.0
+    ξηζ = tile_ref[li] / 4.0   # Normalizované souřadnice
     ξ, η, ζ = ξηζ[1], ξηζ[2], ξηζ[3]
-    # Afinní mapování: p = vmins + (vmaxs - vmins) .* (ξ, η, ζ)
     p = [ vmins[d] + (vmaxs[d] - vmins[d]) * ξηζ[d] for d in 1:3 ]
-    # Kvantizujeme souřadnice
     p_key = quantize(p)
     if haskey(mesh.node_hash, p_key)
-      # Pokud uzel s touto kvantizovanou polohou existuje, použijeme jeho globální index
       local_mapping[li] = mesh.node_hash[p_key]
     else
-      # Jinak vložíme nový uzel do globálního seznamu
       push!(mesh.X, p)
-      # Vypočteme trilineární interpolaci SDF hodnoty
       N000 = (1-ξ) * (1-η) * (1-ζ)
       N100 = ξ     * (1-η) * (1-ζ)
       N010 = (1-ξ) * η     * (1-ζ)
@@ -131,11 +137,8 @@ function process_cell_A15!(mesh::BlockMesh, i::Int, j::Int, k::Int)
     end
   end
   
-  # Sestavíme tetraedrické elementy podle A15 schématu
-  # Upozorňuji: tetra_connectivity definuje indexy jako 0-based – proto přičítáme 1.
   for tet in tetra_connectivity
-    global_tet = [ local_mapping[li + 1] for li in tet ]
-    # Přidáme tetraedr, pokud alespoň jeden z jeho uzlů má SDF ≥ 0
+    global_tet = [ local_mapping[li] for li in tet ]
     tet_sdf = [ mesh.node_sdf[idx] for idx in global_tet ]
     if any(x -> x >= 0, tet_sdf)
       push!(mesh.IEN, global_tet)
@@ -144,14 +147,60 @@ function process_cell_A15!(mesh::BlockMesh, i::Int, j::Int, k::Int)
 end
 
 # ----------------------------
-# Funkce pro sloučení duplicitních uzlů po tvorbě sítě
+# NOVÉ: Funkce diskretizace buňky pomocí Schlafli orthoscheme
+# ----------------------------
+function process_cell_Schlafli!(mesh::BlockMesh, i::Int, j::Int, k::Int)
+  # Získáme SDF hodnoty osmi vrcholů buňky
+  sdf_values = get_cell_sdf_values(mesh, i, j, k)
+  if !any(x -> x >= 0, sdf_values)
+    return
+  end
+  
+  # Vytvoříme lokální mapování přímo pro 8 uzlů buňky
+  local_mapping = Dict{Int,Int}()
+  cell_nodes = [
+    mesh.grid[i,   j,   k],     # Node 1: front-bottom-left
+    mesh.grid[i+1, j,   k],       # Node 2: front-bottom-right
+    mesh.grid[i+1, j+1, k],       # Node 3: front-top-right
+    mesh.grid[i,   j+1, k],       # Node 4: front-top-left
+    mesh.grid[i,   j,   k+1],     # Node 5: back-bottom-left
+    mesh.grid[i+1, j,   k+1],     # Node 6: back-bottom-right
+    mesh.grid[i+1, j+1, k+1],     # Node 7: back-top-right
+    mesh.grid[i,   j+1, k+1]      # Node 8: back-top-left
+  ]
+  
+  # Pořadí SDF hodnot odpovídá pořadí vrcholů (viz get_cell_sdf_values)
+  for li in 1:8
+    p = cell_nodes[li]
+    p_key = quantize(p)
+    if haskey(mesh.node_hash, p_key)
+      local_mapping[li] = mesh.node_hash[p_key]
+    else
+      push!(mesh.X, p)
+      push!(mesh.node_sdf, sdf_values[li])
+      local_index = length(mesh.X)
+      local_mapping[li] = local_index
+      mesh.node_hash[p_key] = local_index
+    end
+  end
+  
+  # Sestavíme tetraedry podle schématu Schlafli
+  for tet in schlafli_tet_connectivity
+    global_tet = [ local_mapping[li] for li in tet ]
+    tet_sdf = [ mesh.node_sdf[idx] for idx in global_tet ]
+    if any(x -> x >= 0, tet_sdf)
+      push!(mesh.IEN, global_tet)
+    end
+  end
+end
+
+# ----------------------------
+# Funkce pro sloučení duplicitních uzlů po tvorbě sítě (nezměněno)
 # ----------------------------
 function merge_duplicate_nodes!(mesh::BlockMesh, tol::Float64=TOL)
   new_nodes = Vector{Vector{Float64}}()
   new_node_sdf = Vector{Float64}()
-  # Mapujeme starý index na nový index
   node_map = Dict{Int, Int}()
-  # Použijeme nový slovník pro sloučení – klíč jsou kvantizované souřadnice
   global_hash = Dict{NTuple{3,Float64}, Int}()
   for i in 1:length(mesh.X)
     p = mesh.X[i]
@@ -166,60 +215,18 @@ function merge_duplicate_nodes!(mesh::BlockMesh, tol::Float64=TOL)
       global_hash[p_key] = new_index
     end
   end
-  # Aktualizujeme konektivitu tetraedrů
   for i in 1:length(mesh.IEN)
     mesh.IEN[i] = [ node_map[old] for old in mesh.IEN[i] ]
   end
   mesh.X = new_nodes
   mesh.node_sdf = new_node_sdf
-  # Můžeme také aktualizovat node_map, pokud je dále využíván
   mesh.node_map = node_map
   @info "Po slučování duplicit: $(length(mesh.X)) uzlů"
 end
 
 # ----------------------------
-# Upravená funkce generování sítě
-# ----------------------------
-function generate_mesh!(mesh::BlockMesh)
-  empty!(mesh.X)
-  empty!(mesh.IEN)
-  empty!(mesh.node_sdf)
-  empty!(mesh.node_map)
-  empty!(mesh.cell_center_map)
-  empty!(mesh.node_hash)
-  
-  # Diskretizujeme každou buňku
-  for i in 1:mesh.nx-1
-    for j in 1:mesh.ny-1
-      for k in 1:mesh.nz-1
-        process_cell_A15!(mesh, i, j, k)
-      end
-    end
-  end
-  
-  # Případně lze spustit i funkci cleanup_unused_nodes! pokud by se objevily nevyužité uzly
-  cleanup_unused_nodes!(mesh)
-  # Sloučíme případné duplicitní uzly (které mohly vzniknout mezi sousedními buňkami)
-  merge_duplicate_nodes!(mesh, TOL)
-  
-  create_INE!(mesh)
-  @info "Vytvořeno $(length(mesh.X)) uzlů a $(length(mesh.IEN)) tetraedrů"
-end
-
-# Export do VTK (nezměněno)
-function export_mesh_vtk(mesh::BlockMesh, filename::String)
-  npoints = length(mesh.X)
-  points = zeros(Float64, 3, npoints)
-  for i in 1:npoints
-    points[:, i] = mesh.X[i]
-  end
-  cells = [MeshCell(VTKCellTypes.VTK_TETRA, tet) for tet in mesh.IEN]
-  vtkfile = vtk_grid(filename, points, cells)
-  vtk_point_data(vtkfile, mesh.node_sdf, "sdf")
-  vtk_save(vtkfile)
-end
-
 # Vyčištění nepoužitých uzlů a přeindexování konektivity (nezměněno)
+# ----------------------------
 function cleanup_unused_nodes!(mesh::BlockMesh)
   @info "Počet uzlů před vyčištěním: $(length(mesh.X))"
   used_nodes = Set{Int64}()
@@ -244,7 +251,9 @@ function cleanup_unused_nodes!(mesh::BlockMesh)
   @info "Počet uzlů po vyčištění: $(length(mesh.X))"
 end
 
-# Vytvoří INE – inverzní konektivitu (nezměněno)
+# ----------------------------
+# Vytvoření inverzní konektivity (nezměněno)
+# ----------------------------
 function create_INE!(mesh::BlockMesh)
   mesh.INE = [Vector{Int64}() for _ in 1:length(mesh.X)]
   for (elem_id, element) in enumerate(mesh.IEN)
@@ -255,11 +264,64 @@ function create_INE!(mesh::BlockMesh)
   return mesh
 end
 
-# Hlavní běh – vytvoříme síť a exportujeme ji do souboru
-mesh = BlockMesh()
-@time generate_mesh!(mesh)
-export_mesh_vtk(mesh, "A15_block-mesh.vtu")
+# ----------------------------
+# Upravená funkce generování sítě s možností volby schématu
+# ----------------------------
+function generate_mesh!(mesh::BlockMesh, scheme::String)
+  empty!(mesh.X)
+  empty!(mesh.IEN)
+  empty!(mesh.node_sdf)
+  empty!(mesh.node_map)
+  empty!(mesh.cell_center_map)
+  empty!(mesh.node_hash)
+  
+  for i in 1:mesh.nx-1
+    for j in 1:mesh.ny-1
+      for k in 1:mesh.nz-1
+        if scheme == "A15"
+          process_cell_A15!(mesh, i, j, k)
+        elseif scheme == "Schlafli"
+          process_cell_Schlafli!(mesh, i, j, k)
+        else
+          error("Neznámé schéma: $scheme")
+        end
+      end
+    end
+  end
+  
+  cleanup_unused_nodes!(mesh)
+  merge_duplicate_nodes!(mesh, TOL)
+  
+  create_INE!(mesh)
+  @info "Vytvořeno $(length(mesh.X)) uzlů a $(length(mesh.IEN)) tetraedrů"
+end
 
+# ----------------------------
+# Export do VTK (nezměněno)
+# ----------------------------
+function export_mesh_vtk(mesh::BlockMesh, filename::String)
+  npoints = length(mesh.X)
+  points = zeros(Float64, 3, npoints)
+  for i in 1:npoints
+    points[:, i] = mesh.X[i]
+  end
+  cells = [MeshCell(VTKCellTypes.VTK_TETRA, tet) for tet in mesh.IEN]
+  vtkfile = vtk_grid(filename, points, cells)
+  vtk_point_data(vtkfile, mesh.node_sdf, "sdf")
+  vtk_save(vtkfile)
+end
+
+# ----------------------------
+# Hlavní běh – vytvoříme síť a exportujeme ji do souboru
+# ----------------------------
+mesh = BlockMesh()
+# Volba schématu: "A15" nebo "Schlafli"
+@time generate_mesh!(mesh, "Schlafli")
+export_mesh_vtk(mesh, "block-mesh.vtu")
+
+
+mesh.INE
+mesh.IEN
 mesh.X
 # mesh.X 282703-element
 # mesh.IEN 475215-element
