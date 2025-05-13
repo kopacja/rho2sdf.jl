eb740c334de306a58aecda9155deaf347bd5cdfd
jezekon <78500480+jezekon@users.noreply.github.com>
2025-05-13 16:31:11 +0200

    new name sdfOnTriangularMeshTest

--- test/SignedDistancesTest/TriangularMeshUtilsTest.jl
+++ test/SignedDistancesTest/TriangularMeshUtilsTest.jl
@@ -13,20 +13,20 @@ using Rho2sdf.SignedDistances
 
     # center
     x_center = (x₁ + x₂ + x₃)./3
-    @test barycentricCoordinates(x₁, x₂, x₃, n, x_center) ≈ [0.333, 0.333, 0.333] atol=0.001 
+    @test SignedDistances.barycentricCoordinates(x₁, x₂, x₃, n, x_center) ≈ [0.333, 0.333, 0.333] atol=0.001 
 
     # vertexes
     x_v1 = [5., 1., 4.]
-    @test barycentricCoordinates(x₁, x₂, x₃, n, x_v1) ≈ [1., 0., 0.]
+    @test SignedDistances.barycentricCoordinates(x₁, x₂, x₃, n, x_v1) ≈ [1., 0., 0.]
 
     x_v2 = [2., 3., 4.]
-    @test barycentricCoordinates(x₁, x₂, x₃, n, x_v2) ≈ [0., 1., 0.]
+    @test SignedDistances.barycentricCoordinates(x₁, x₂, x₃, n, x_v2) ≈ [0., 1., 0.]
 
     x_v3 = [3., 2., 4.]
-    @test barycentricCoordinates(x₁, x₂, x₃, n, x_v3) ≈ [0., 0., 1.]
+    @test SignedDistances.barycentricCoordinates(x₁, x₂, x₃, n, x_v3) ≈ [0., 0., 1.]
     
     x_out = [10., 10., 10.]
-    @test minimum(barycentricCoordinates(x₁, x₂, x₃, n, x_out)) < 0. 
+    @test minimum(SignedDistances.barycentricCoordinates(x₁, x₂, x₃, n, x_out)) < 0. 
 end
 
 end
